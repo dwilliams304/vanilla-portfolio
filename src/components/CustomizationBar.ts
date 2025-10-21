c96a38369eb24b7045e6729498a1e3e070502329
Davis W <d.williams0434@gmail.com>
2025-10-21 16:21:23 -0400

    more basic setup

---
+++ src/components/CustomizationBar.ts
@@ -0,0 +1,8 @@
+export function CustomizationBar(){
+    const bar = document.getElementById("customization-bar");
+    if(!bar) return;
+
+    bar.innerHTML = `
+        <p>Placeholder customization bar!</p>
+    `
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import "./style.css";
+import { CustomizationBar } from "./components/CustomizationBar";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>daviswilliams.dev</h1>
-  </div>
-`
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     // <h1>daviswilliams.dev</h1>
+//   </div>
+// `
+
+CustomizationBar();
